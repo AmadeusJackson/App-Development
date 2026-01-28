dca02e52da43e6ae9ad14b1c6f2b4d679d475fd0
Amadeus Jackson <nottherealmozart@gmail.com>
2026-01-28 10:19:11 -0500

    worked on teh badges

---
+++ Focus/Focus/Focus/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Focus
+//
+//  Created by Amadeus Jackson on 1/26/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
